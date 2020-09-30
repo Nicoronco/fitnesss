8d57d71fb96224bb6523be6f9669569005d4d153
Nicoronco <72089352+Nicoronco@users.noreply.github.com>
2020-09-30 17:18:27 -0300

    Intento APP
    Ejer

---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles/Welcome.css'
+
+function Welcome(props){
+  return(
+    <div class="container">
+    <div class="Fitness-User-Info">
+      <h1>Hello {props.username}!</h1>
+      <p>Let´s workout to get someone gains</p>
+    </div>
+  </div>
+  )
+
+}
+
+export default Welcome
---
+++ src/pages/Exercises.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Card from '../components/Card'
+import Welcome from './components/Welcome'
+
+class Exercises extends React.Component {
+
+}
+export default Exercises
